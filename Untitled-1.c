0fb1fe832cb030c613136712450752b56b4c927c
VyasAaryan2890 <139629153+VyasAaryan2890@users.noreply.github.com>
2023-08-18 22:41:32 +0530

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j,n;
+    printf("enter numer");
+    scanf("%d",n);
+    for(i=0;i<;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("* ");
+        }
+    }
+    return 0;
+}
